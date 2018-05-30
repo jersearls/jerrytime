3ad840f8dd9d214ec6b08087b8a984be1be4dcbb
jersearls <searls@me.com>
2018-05-29 22:58:46 -0400

    refactor title

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,22 +1,25 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+  def setup
+    @base_title = "JerryTime"
+  end
+
   test "should get home" do
     get static_pages_home_url
     assert_response :success
-    assert_select "title", "Home | Jerrytime" 
-    end
+    assert_select "title", "Home | #{@base_title}" 
   end
 
   test "should get resume" do
     get static_pages_resume_url
     assert_response :success
-    assert_select "title", "Resume | Jerrytime"
+    assert_select "title", "Resume | #{@base_title}"
   end
 
   test "should get about" do
     get static_pages_about_url
     assert_response :success
-    assert_select "title", "About | Jerrytime"
+    assert_select "title", "About | #{@base_title}"
   end
 end
